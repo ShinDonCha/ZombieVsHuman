ce36de37b9e2fe5310cf7308048d5138513975f2
hyuk-jun <gurwkd14@naver.com>
2022-08-04 10:54:49 +0900

    GlobalData 스크립트 추가
    헬멧 및 방탄복 아이템 추가

--- Scripts/GlobalData.cs
+++ Scripts/GlobalData.cs
@@ -17,9 +17,11 @@ public enum ItemName
     M16 = 1,
     K2 = 2,
     Ak47 = 3,
-    SniperRifle = 4,       
-    ItemCount = 5,
-    Null    
+    SniperRifle = 4,
+    Armour = 5,
+    Helmet = 6,
+    ItemCount = 7,
+    Null
 }
 
 public class ItemInfo //각 Item 정보
@@ -39,7 +41,7 @@ public class ItemInfo //각 Item 정보
     public Sprite m_iconImg = null;   //캐릭터 아이템에 사용될 이미지
     public Vector2 m_iconSize = Vector2.one;  //아이템 이미지의 가로 사이즈, 세로 사이즈
     public string m_itemEx = "";      //아이템 설명
-    
+
 
     public void SetType(ItemName itemName)
     {
@@ -51,7 +53,7 @@ public class ItemInfo //각 Item 정보
             m_iconSize.x = 0.85f;   //Content의 cell크기 0.85배
             m_iconSize.y = 1.0f;     //Content의 cell크기
             m_maganize = 100;
-            m_damage = 20;            
+            m_damage = 20;
             m_attackDelay = 0.5f;          //공격 딜레이
             m_isDropped = false;
             m_itemEx = "쓰임에 따라 스포츠가 될지 , 느와르물이 될지 정해진다. 지금은 후자일지도..";
@@ -122,6 +124,25 @@ public class ItemInfo //각 Item 정보
             m_itemEx = "원샷 원킬? 생각보다 어려울껄";
             //m_IconImg = Resources.Load("IconImg/m0423", typeof(Sprite)) as Sprite;
         }
+        else if (itemName == ItemName.Armour)
+        {
+            m_name = "Armour";
+            m_itType = ItemType.Armor;
+            m_maganize = 30;
+            m_isDropped = false;
+
+            m_itemEx = "군인들이 사용하던 방탄복. 입으면 좀비들에게 살살 맞을 지도?";
+        }
+        else if (itemName == ItemName.Helmet)
+        {
+            m_name = "Helmet";
+            m_itType = ItemType.HeadGear;
+            m_maganize = 15;
+            m_isDropped = false;
+
+            m_itemEx = "헬멧안에서 땀냄새가 좀 나지만, 안쓰는거 보단 나을 껄";
+        }
+
         //else if (itemType == ItemType.Fist)
         //{
         //    m_name = "Fist";
@@ -156,32 +177,33 @@ public class GlobalValue
     public static int g_equipFullSlotCount = 3;           //equipPanel에 넣어줄 슬롯 개수
 
     public static Dictionary<ItemName, ItemInfo> g_itemDic = new Dictionary<ItemName, ItemInfo>();      //전체 아이템 정보를 담을 딕셔너리
-        
+
     public static void InitData()       //딕셔너리에 아이템 정보 저장
-    {        
+    {
         if (0 < g_itemDic.Count)
             return;
 
         ItemInfo a_ItemList;
         for (int ii = 0; ii < (int)ItemName.ItemCount; ii++)
-        {            
+        {
             a_ItemList = new ItemInfo();
             a_ItemList.SetType((ItemName)ii);
-            g_itemDic.Add((ItemName)ii, a_ItemList);                       
+            g_itemDic.Add((ItemName)ii, a_ItemList);
         }
-        
+
         for (int invenadd = 0; invenadd < g_invenFullSlotCount; invenadd++)         //인벤토리 슬롯의 개수만큼 리스트 생성
         {
             a_ItemList = new ItemInfo();
             g_userItem.Add(a_ItemList);
-        }             
-        
+        }
+
         for (int equipadd = 0; equipadd < 3; equipadd++)
         {
-            a_ItemList = new ItemInfo();            
+            a_ItemList = new ItemInfo();
             g_equippedItem.Add(a_ItemList);
         }
     }//public static void InitData()
 
 }
+
 
